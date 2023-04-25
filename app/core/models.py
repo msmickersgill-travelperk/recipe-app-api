d0bf360c2c048cef1b8717d8ff4434830e241bbc
Maria <maria.smickersgill@travelperk.com>
2023-04-25 12:50:34 +0200

    Add documentation and other stuff

--- app/core/models.py
+++ app/core/models.py
@@ -14,11 +14,23 @@ class UserManager(BaseUserManager):
     """Manager for user"""
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=self.normalize_user(email, **extra_fields))
+        if not email:
+            raise ValueError('User must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
